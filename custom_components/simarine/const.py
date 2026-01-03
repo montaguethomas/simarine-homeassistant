3c3dc88ba6458e5ffafae4f22970016f5a118a8b
Thomas Montague <montague.thomas@lmbbox.com>
2026-01-03 16:12:35 -0500

    Adjust scan interval constants for simarine

--- custom_components/simarine/const.py
+++ custom_components/simarine/const.py
@@ -7,5 +7,5 @@ DOMAIN: Final = "simarine"
 CONF_TCP_PORT: Final = "tcp_port"
 CONF_UDP_PORT: Final = "udp_port"
 
-DEFAULT_SCAN_INTERVAL: Final = 15
-MIN_SCAN_INTERVAL: Final = 5
+DEFAULT_SCAN_INTERVAL: Final = 5
+MIN_SCAN_INTERVAL: Final = 1
